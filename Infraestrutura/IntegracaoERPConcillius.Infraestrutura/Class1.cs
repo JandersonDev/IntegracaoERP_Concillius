13279e95f1506ae4f267ae30666d2fff013554c4
JandersonDev <jandersondevops@gmail.com>
2017-03-22 18:07:00 -0300

    Commit Inicial

---
+++ Infraestrutura/IntegracaoERPConcillius.Infraestrutura/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegracaoERPConcillius.Infraestrutura
+{
+    public class Class1
+    {
+    }
+}
---
+++ Servico/ApiIntegracaoERPConcillius/Controllers/GravaVendaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ApiIntegracaoERPConcillius.Controllers
+{
+    public class GravaVendaController : Controller
+    {
+        // GET: GravaVenda
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
